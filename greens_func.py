520adef84f58f02cd8b5b0b91f9de35f8222c7dc
Juan Mendez <som.felipe66@gmail.com>
2022-02-14 13:56:20 -0500

    added calculation of the greens function

--- greens_func.py
+++ greens_func.py
@@ -1,36 +1,68 @@
 import numpy as np
 from scipy.linalg import svd
+from scipy.linalg import inv
 import sys
 import Propagators
 import Hamiltonian
 import Auxfield
 import stab
-
+import time
 class Geq:
     def __init__(self, stringops ):
-        pass
+        self.stringops=stringops
+        self.Glist=self.construct_G()
+
+    def inver_IpB(self,BB):
+        [U,D,V]=svd(BB)
+        second_mat=np.conj(U.T)@np.conj(V.T)+np.diag(D)
+        [Up,Dp,Vp]=svd(second_mat)
+        return (np.conj((Vp@V).T)/Dp)@np.conj((U@Up).T)
+
+    def naive_inver_IpB(self,BB):
+        nainv=inv(np.eye(np.shape(BB)[0])+BB)
+        return nainv 
+
+    def construct_G(self):
+        Glist=[]
+        Ntau=self.stringops.Ops.gamma.Ntau
+        for tau in range(Ntau):
+            B1=self.stringops.Opmult_stab_LtoR(tau,0)
+            B2=self.stringops.Opmult_stab_LtoR(Ntau-1,tau)
+            BB=B1@B2
+            a2= np.eye(4)+np.reshape(np.arange(16),[4,4])
+            # print(np.min( np.abs(eigvals(a2))) )
+            print(np.mean(np.abs(self.inver_IpB(a2)-self.naive_inver_IpB(a2))))
+
+            Glist.append(BB)
+        return Glist
+
 
 
 
 def main()->int:
-    Nsites=20
-    Ntau=20
-    dtau=0.01
-    Nwrap=1
+    Nsites=25
+    Beta=5
+    dtau=0.1
+    Ntau=int(Beta/dtau)
+    print("ntau is",Ntau)
+    
+    Nwrap=10
     mu=0
-    U=10
+    U=4
     np.random.seed(1)
     ht=Hamiltonian.Hopping(Nsites,dtau,mu)
     hv=Hamiltonian.Vint(dtau,U)
     gamma=Auxfield.AuxField(Ntau,Nsites)
     Bp=Propagators.Btau_s(ht, hv, gamma, 1)
-    Bm=Propagators.Btau_s(ht, hv, gamma, -1)
-    Bsp=stab.StringOps(Bp,Nwrap)
-    Bsm=stab.StringOps(Bm,Nwrap)
-    stab=Bsp.Opmult_stab_LtoR(0,Ntau-1)
-    stab=Bsm.Opmult_stab_LtoR(0,Ntau-1)
+    # Bm=Propagators.Btau_s(ht, hv, gamma, -1)
 
-    Geqp=Geq()
+    Bsp=stab.StringOps(Bp,Nwrap)
+    
+    s=time.time()
+    Geqp=Geq(Bsp)
+    e=time.time()
+    print(e-s)
+
     return 0
 
 if __name__=='__main__':
--- stab.py
+++ stab.py
@@ -4,56 +4,39 @@ import sys
 import Propagators
 import Hamiltonian
 import Auxfield
+import time
 
 class StringOps:
     def __init__(self, Ops, Nwrap):
         self.Ops=Ops
         self.Nwrap=Nwrap
 
-    def Opmult_stab2_LtoR(self, intau, fintau):
-        
-        if intau<fintau:
-            Res=self.Ops.Bs[intau]
-            for tau in range(intau+1,fintau):
-                if tau%self.Nwrap!=0:
-                    Res=self.Ops.Bs[tau]@Res
-                else:
-                    [U,D,V]=svd(Res)
-                    temp=(self.Ops.Bs[tau]@U)*D
-                    [Up,Dp,Vp]=svd(temp)
-                    VpV=Vp@V
-                    UpDp=Up*Dp
-                    Res=UpDp@VpV
-        else:
-            print(f'setting initial time as {fintau} and final time as {intau}')
-            Res=self.Ops.Bs[fintau]
-            for tau in range(fintau+1,intau):
-                if tau%self.Nwrap!=0:
-                    Res=self.Ops.Bs[tau]@Res
-                else:
-                    [U,D,V]=svd(Res)
-                    temp=(self.Ops.Bs[tau]@U)*D
-                    [Up,Dp,Vp]=svd(temp)
-                    VpV=Vp@V
-                    UpDp=Up*Dp
-                    Res=UpDp@VpV
-        return Res
+    def stabmult_svd(self,A,B):
+        [U,D,V]=svd(B)
+        temp=(A@U)*D
+        [Up,Dp,Vp]=svd(temp)
+        VpV=Vp@V
+        UpDp=Up*Dp
+        return UpDp@VpV
+
         
     def Opmult_stab_LtoR(self, in_tau, fin_tau):
         
         if in_tau<fin_tau:
             intau=in_tau
             fintau=fin_tau
-        else:
+        elif(in_tau>fin_tau):
             intau=fin_tau
             fintau=in_tau
+        else:
+            return self.Ops.Bs[in_tau]
 
         shape_mat=np.shape(self.Ops.Bs[intau])
         Res=np.eye(shape_mat[0])
         tautot=fintau-intau
         groups=tautot//self.Nwrap
         residue=tautot%self.Nwrap
-        print('groups',groups,groups*self.Nwrap, tautot,)
+        # print('groups',groups,groups*self.Nwrap, tautot,)
         
         for group in range(0,groups):
             # print('group_c',group)
@@ -61,23 +44,14 @@ class StringOps:
             temp_prod=self.Ops.Bs[temp_ind]
             for tau in range(intau+1+(group)*self.Nwrap,intau+(group+1)*self.Nwrap):
                     temp_prod=self.Ops.Bs[tau]@temp_prod
-            [U,D,V]=svd(Res)
-            temp=(temp_prod@U)*D
-            [Up,Dp,Vp]=svd(temp)
-            VpV=Vp@V
-            UpDp=Up*Dp
-            Res=UpDp@VpV
+            Res=self.stabmult_svd(temp_prod,Res)
+            
 
         if residue>0:
             temp_prod=self.Ops.Bs[groups*self.Nwrap]
-            for tau in range(groups*self.Nwrap+1,fintau):
+            for tau in range(groups*self.Nwrap+1,fintau+1): #fintau+1 since we want to include the last matrix
                     temp_prod=self.Ops.Bs[tau]@temp_prod
-            [U,D,V]=svd(Res)
-            temp=(temp_prod@U)*D
-            [Up,Dp,Vp]=svd(temp)
-            VpV=Vp@V
-            UpDp=Up*Dp
-            Res=UpDp@VpV
+            Res=self.stabmult_svd(temp_prod,Res)
         return Res
                         
         
@@ -85,7 +59,7 @@ class StringOps:
         
         if intau<fintau:
             Res=self.Ops.Bs[intau]
-            for tau in range(intau+1,fintau):
+            for tau in range(intau+1,fintau+1):
                 Res=self.Ops.Bs[tau]@Res
   
         else:
@@ -102,7 +76,7 @@ def main()->int:
     Beta=5
     dtau=0.1
     Ntau=int(Beta/dtau)
-    print(Ntau)
+    print("ntau is",Ntau)
     
     Nwrap=10
     mu=0
@@ -112,23 +86,35 @@ def main()->int:
     hv=Hamiltonian.Vint(dtau,U)
     gamma=Auxfield.AuxField(Ntau,Nsites)
     Bp=Propagators.Btau_s(ht, hv, gamma, 1)
-    Bm=Propagators.Btau_s(ht, hv, gamma, -1)
+    # Bm=Propagators.Btau_s(ht, hv, gamma, -1)
 
-    Bs=StringOps(Bp,Nwrap)
+    Bsp=StringOps(Bp,Nwrap)
     
-    stab=Bs.Opmult_stab2_LtoR(0,Ntau)
-    nostab=Bs.Opmult_LtoR(0,Ntau)
+    nostab=Bsp.Opmult_LtoR(0,Ntau)
     
-    Bs=StringOps(Bp,Nwrap)
-    stab2=Bs.Opmult_stab_LtoR(0,Ntau)
+    Bsp=StringOps(Bp,Nwrap)
+    stab=Bsp.Opmult_stab_LtoR(0,Ntau)
+
+    for Nw in range(1,Ntau):
+        Bsp=StringOps(Bp,Nw)
+        s=time.time()
+        stab=Bsp.Opmult_stab_LtoR(0,Ntau)
+        e=time.time()
+        print(Nw,e-s, np.mean(np.abs(stab)))
+
 
 
     print(1,np.mean(np.abs(stab)))
     print(2,np.mean(np.abs(nostab)))
-    print(3,np.mean(np.abs(stab2)))
     print('dif',np.mean(np.abs(nostab-stab)))
-    print('dif',np.mean(np.abs(stab2-stab)))
-    print('dif',np.mean(np.abs(stab2-nostab)))
+
+    #testing equal index
+    i=10
+    stab=Bsp.Opmult_stab_LtoR(i,i)
+    print(np.mean(np.abs(stab)))
+    print(np.mean(np.abs(Bp.Bs[i])))
+
+
     return 0
 
 
